f818706848f9018e4dfd0d9187324ee7bc913434
Daniel Bruce <42590607+slinksoft@users.noreply.github.com>
2020-11-17 21:37:48 -0500

    Update tests/test_core.py
    Co-authored-by: Marvin Torres <mtorr068@fiu.edu>

--- tests/test_core.py
+++ tests/test_core.py
@@ -18,22 +18,23 @@ class TestEDPF(unittest.TestCase):
         new_mock_EDPF.search(G, 10, 'a', 'b')
         new_mock_EDPF.search.assert_called()
         
-    @patch('exactdelaypathfinder.core.ExactDelayPathfinder', autospec=True)
-    def test_search(self, mock_EDPF):
+    @patch.object(ExactDelayPathfinder, '_search')
+    def test_search(self, mock_search):
         """ Tests the _search function, the DFS algorithm (recursive method) 
         
             Parameters:
                mock_EDPF: Mock object reference of ExactDelayPathfinder
         """
-        new_mock_EDPF = mock_EDPF.return_value
-        new_mock_EDPF._search.return_value = 86
+        new_EDPF = ExactDelayPathfinder()
+        mock_search.return_value = []
+
         G = Mock()
+        G.nodes = []
         delay = 64;
         start = 'a'
         end = 'b'
-        new_mock_EDPF._search(G, 64, 'a', 'b', [])
-        new_mock_EDPF._search.assert_called_with(G, delay, start, end, visits=[])
-
+        new_EDPF.search(G, 64, 'a', 'b')
+        new_EDPF._search.assert_called()
     def test_search_graph_Error(self):
         """ Tests the search function attribute errors for the graph input parameter 
         
